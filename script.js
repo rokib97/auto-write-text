7b58386e431fa39e9d760261833d9d98c472273b
rokib97 <hasanrokib1997@gmail.com>
2022-02-21 14:50:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const text =
+  "আন্তর্জাতিক মাতৃভাষা দিবসে সকল মহান ভাষা শহীদদের প্রতি রইলো বিনম্র শ্রদ্ধা।";
+
+let index = 0;
+
+function writeText() {
+  document.body.innerText = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) {
+    index = 0;
+  }
+}
+
+setInterval(writeText, 100);
